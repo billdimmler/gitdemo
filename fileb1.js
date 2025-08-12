a03cdaa0bab53e2256bbe776dc7621e271bbe8d6
billdimmler <dimmler@protonmail.com>
2025-08-11 22:16:48 -0400

    b1

--- fileb1.js
+++ fileb1.js
@@ -1,3 +1,9 @@
 main branch
 Annie Wannie
  Bonnie Wonnie
+// generating a random number
+const a = Math.random() * (10-1) + 1
+console.log();
+// generating a random number
+const a = Math.random() * (10-1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
